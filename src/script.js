cb2fbc24587438090f8f72e3c8b3f166146dba43
gegi <35646675+Gary21435@users.noreply.github.com>
2025-05-18 17:28:17 -0700

    Implement adding and checking default projects

--- src/script.js
+++ src/script.js
@@ -9,7 +9,20 @@ import {
 } from "./logic/todoManager.js";
 import { displayProject } from "./ui/dom.js";
 
+const newProjButton = document.querySelector("#new-project");
+
 // Make default project to start things off
 const defProject = defaultProject();
 displayProject(defProject);
 
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("project-check")) {
+        const p = e.target.nextElementSibling;
+        p.classList.toggle('line-through');
+    }
+});
+
+newProjButton.addEventListener("click", (e) => {
+    const defProject = defaultProject();
+    displayProject(defProject);
+});
--- src/ui/dom.js
+++ src/ui/dom.js
@@ -1,9 +1,17 @@
 const projects_container = document.querySelector(".projects-container");
 
 export function displayProject(project) {
-    let projectHeader = document.createElement("div");
-    projectHeader.className = "project";
+    let container = document.createElement("div");
+
+    let projectHeader = document.createElement("p");
+    container.className = "project";
     projectHeader.textContent = project.name;
 
-    projects_container.appendChild(projectHeader);
+    let checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+    checkbox.className = 'project-check';
+    container.appendChild(checkbox);
+    container.appendChild(projectHeader);
+
+    projects_container.append(container);
 }
